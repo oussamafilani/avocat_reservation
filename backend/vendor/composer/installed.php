9b9e4c3bb7b7cb248f9b1181af5785a7614ca4cf
Oussama Filani <42185573+filani07@users.noreply.github.com>
2021-06-18 14:08:29 +0100

    yep

---
+++ backend/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'ac94292941f831c0c14a8ce0c3350bea7b70f963',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'ac94292941f831c0c14a8ce0c3350bea7b70f963',
+            'dev_requirement' => false,
+        ),
+        'firebase/php-jwt' => array(
+            'pretty_version' => 'v5.3.0',
+            'version' => '5.3.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../firebase/php-jwt',
+            'aliases' => array(),
+            'reference' => '3c2d70f2e64e2922345e89f2ceae47d2463faae1',
+            'dev_requirement' => false,
+        ),
+    ),
+);
