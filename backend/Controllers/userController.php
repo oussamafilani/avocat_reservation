b41899bd1761187648a235e72febc9cb943d8662
Oussama Filani <42185573+filani07@users.noreply.github.com>
2021-06-19 23:14:37 +0100

    yep

--- backend/Controllers/userController.php
+++ backend/Controllers/userController.php
@@ -82,8 +82,102 @@ class userController
     }
     public function getUserInfo()
     {
+
+        //check  token
+        $this->User->token = $this->data->token;
+        $this->chekToken = $this->User->CheckToken();
+
+        $this->User->id_client = $this->data->id_client;
+
+        // User query
+        $result = $this->User->getInfo();
+        // Get row count
+        $num = $result->rowCount();
+
+        // Check if any User
+        if ($this->chekToken) {
+            if ($num > 0) {
+                // Post array
+                $posts_arr = array();
+                $appointment_data = array();
+
+                while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+                    extract($row);
+
+                    $client_info = ['client_info' => array(
+                        'nom_client' => $nom_client,
+                        'prenom_client' => $prenom_client,
+                        'profession' => $profession,
+                        'age_client' => $age_client,
+                        'cin' => $cin,
+                    )];
+
+                    $post_item = array(
+                        'id_appointment' => $id_appointment,
+                        'date' => $date,
+                        'sujet' => $sujet,
+                        'd_hour' => $d_hour,
+                        'f_hour' => $f_hour,
+                    );
+
+                    // Push to "data"
+                    array_push($appointment_data, $post_item);
+                }
+                $client_info['client_info']['appointments'] = $appointment_data;
+
+                array_push($posts_arr, $client_info);
+
+                // Turn to JSON & output
+                echo json_encode($posts_arr);
+            } else {
+                // No User
+                echo json_encode(
+                    array('message' => 'No User Found')
+                );
+            }
+        } else {
+            echo json_encode(
+                array('message' => 'Token Not Valid')
+            );
+        }
     }
     public function readUser()
     {
+
+        // User query
+        $result = $this->User->read();
+        // Get row count
+        $num = $result->rowCount();
+
+        // Check if any User
+        if ($num > 0) {
+            // Post array
+            $posts_arr = array();
+
+            while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+                extract($row);
+
+                $post_item = array(
+                    'id_client' => $id_client,
+                    'nom_client' => $nom_client,
+                    'prenom_client' => $prenom_client,
+                    'profession' => $profession,
+                    'age_client' => $age_client,
+                    'cin' => $cin,
+                    'id_user' => $id_user,
+                );
+
+                // Push to "data"
+                array_push($posts_arr, $post_item);
+            }
+
+            // Turn to JSON & output
+            echo json_encode($posts_arr);
+        } else {
+            // No User
+            echo json_encode(
+                array('message' => 'No User Found')
+            );
+        }
     }
 }
