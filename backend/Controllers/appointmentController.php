17747231ff2f46e60c3fd7cd55041e9ac4c056a1
Oussama Filani <42185573+filani07@users.noreply.github.com>
2021-06-19 22:50:32 +0100

    yep

--- backend/Controllers/appointmentController.php
+++ backend/Controllers/appointmentController.php
@@ -85,4 +85,126 @@ class appointmentController
             );
         }
     }
+
+    public function getClientAppointment()
+    {
+
+        //check  token
+        $this->Appointment->token = $this->data->token;
+        $this->chekToken = $this->Appointment->CheckToken();
+
+        $this->Appointment->id_client = $this->data->id_client;
+
+
+        if ($this->chekToken) {
+            // Appointment query
+            $result = $this->Appointment->getClientAppointment();
+            // Get row count
+            $num = $result->rowCount();
+
+            // Check if any Appointment
+            if ($num > 0) {
+                // Post array
+                $posts_arr = array();
+
+                while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+                    extract($row);
+
+                    $post_item = array(
+                        'id_appointment' => $id_appointment,
+                        'date' => $date,
+                        'sujet' => $sujet,
+                        'id_creneaux' => $id_creneaux,
+                        'id_client' => $id_client,
+                    );
+
+                    // Push to "data"
+                    array_push($posts_arr, $post_item);
+                }
+
+                // Turn to JSON & output
+                echo json_encode($posts_arr);
+            } else {
+                // No Appointment
+                echo json_encode(
+                    array('message' => 'No Appointment Found')
+                );
+            }
+        } else {
+            echo json_encode(
+                array('message' => 'Token Not Valid')
+            );
+        }
+    }
+
+    public function readAppointment()
+    {
+        // Appointment query
+        $result = $this->Appointment->read();
+        // Get row count
+        $num = $result->rowCount();
+
+        // Check if any Appointment
+        if ($num > 0) {
+            // Post array
+            $posts_arr = array();
+
+            while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+                extract($row);
+
+                $post_item = array(
+                    'id_appointment' => $id_appointment,
+                    'date' => $date,
+                    'sujet' => $sujet,
+                    'id_creneaux' => $id_creneaux,
+                    'id_client' => $id_client,
+                );
+
+                // Push to "data"
+                array_push($posts_arr, $post_item);
+            }
+
+            // Turn to JSON & output
+            echo json_encode($posts_arr);
+        } else {
+            // No Appointment
+            echo json_encode(
+                array('message' => 'No Appointment Found')
+            );
+        }
+    }
+
+    public function updateAppointment()
+    {
+
+        //check  token
+        $this->Appointment->token = $this->data->token;
+        $chekToken = $this->Appointment->CheckToken();
+
+
+        // Set ID to update
+        $this->Appointment->id_appointment = $this->data->id_appointment;
+
+        // $Appointment->token = $data->token;
+        $this->Appointment->date = $this->data->date;
+        $this->Appointment->sujet = $this->data->sujet;
+        $this->Appointment->id_creneaux = $this->data->id_creneaux;
+
+        // Update post
+        if ($chekToken) {
+            if ($this->Appointment->update()) {
+                echo json_encode(
+                    array('message' => 'Appointment Updated')
+                );
+            } else {
+                echo json_encode(
+                    array('message' => 'Appointment Not Updated')
+                );
+            }
+        } else {
+            echo json_encode(
+                array('message' => 'Token No Valid')
+            );
+        }
+    }
 }
